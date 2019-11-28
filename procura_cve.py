da1ead24db0a26d9235ab821c8c19a12cb30b010
m47h3u5.0liv31r4 <matheusoliveiratux4me@gmail.com>
2019-11-27 21:44:46 -0300

    its alive :sunglass:

--- procura_cve.py
+++ procura_cve.py
@@ -12,14 +12,15 @@ vuln=CVESearch()
 op=int(input("digite 1 para ir à pagina do fabricante\n\n2 para pesquisar por produto\n\n3 para pesquisar por CVE_id\n\n4 as ultimas cves\n\n"))
 if (op==1):
     vendor= input("digite o nome do fabricante\n")
-    vuln.browse(vendor)
+    print('pagina do vendor {}\n'.format(vuln.browse(vendor)))
 elif (op==2):
     product= input("digite o nome do produto que deseja pesquisar\n")
-    vuln.search(product)
+    print('vulnerabilidades cadastradas para o produto {}\n'.format(vuln.search(product)))
 elif (op==3):
     cvid= input("digite o id que deseja buscar \n")
-    vuln.id(cvid)
+    
+    print('detalhes da CVE solicitada {}\n'.format(vuln.id(cvid)))
 else:
-    vuln.last()
+    print('últimas CVEs cadastradas {}\n'.format(vuln.last()))
 
 
