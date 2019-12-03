76ad668fb97f57702a7299dbd4e8b7034e56da3a
m47h3u5.0liv31r4 <matheusoliveiratux4me@gmail.com>
2019-12-02 21:58:48 -0300

    working on a beutyful return :heart:

--- procura_cve.py
+++ procura_cve.py
@@ -9,23 +9,29 @@ from pycvesearch import CVESearch
 
 #-------------------------------------------------------------
 vuln=CVESearch()
+
+def busca_produto(produto):
+    busca=vuln.search(produto)
+    vid=busca['data'][-1]['id']
+    score=busca['data'][-1]['cvss']
+    resumo=busca['data'][-1]['summary']
+    access=busca['data'][-1]['access']
+    impact=busca['data'][-1]['impact']
+    vproduct=busca['data'][-1]['vulnerable_product']
+    print("Resultado da consulta \n ------------- \n","Resumo: ",resumo,"\n\n CVE=ID: ",vid,"\n\n Acesso: ",access,"\n\n CVSS: ",score,"\n\n Impacto",impact,"\n\n Produtos Atingidos: ",vproduct)  
+
 op=int(input("digite 1 para ir à pagina do fabricante\n\n2 para pesquisar por produto\n\n3 para pesquisar por CVE_id\n\n4 as ultimas cves\n\n"))
 if (op==1):
     vendor= input("digite o nome do fabricante\n")
     print('pagina do vendor {}\n'.format(vuln.browse(vendor)))
 elif (op==2):
-    product= input("digite o nome do produto que deseja pesquisar\n")
-    #print('vulnerabilidades cadastradas para o produto {}\n'.format(vuln.search(product)))
-    busca=vuln.search(product)
-    
-    vid=busca['data'][0]['id']
-    print(vid)       
+   produto=input("digite o produto que deseja pesquisar\n") 
+   busca_produto(produto)       
 elif (op==3):
     cvid= input("digite o id que deseja buscar \n")
     print('detalhes da CVE solicitada {}\n'.format(vuln.id(cvid)))
 else:
     ultimas=vuln.last()
     print('últimas CVEs cadastradas {}\n'.format(vuln.last()))
-
 
 
