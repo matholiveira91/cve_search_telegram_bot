dec719dc94a70d9bc9cbe25d71c2674fc0272eff
jul10l1r4 <jul10l1r4@disroot.org>
2019-12-06 04:37:19 -0300

    :tada: Bot reply

--- bot.py
+++ bot.py
@@ -2,40 +2,43 @@
 # LICENSE GPLV3 
 # PURPUSE TELEGRAM BOT WHOS SEARCH TO CVE's by vendor or product
 
-from telegram.ext import Updater,CommandHandler, MessageHandler, Filters 
+from telegram.ext import Updater, CommandHandler, MessageHandler 
 import logging
 from os import environ 
-import procura_cve
+import serial
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s',level-logging.INFO)
-logger = logging.getLogger(_name_) 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-#COMANDS
+# Commands
+def search(bot, update):
+   text = " ".join(update["message"]["text"].split()[1:])
+   # Chamar funcao
+   message_search = (
+           f"Pesquisa: {text}\n"
+           "Resultado:"
+           )
+   update.message.reply_text(message_search)
 
-def start(bot,update):
-    """Send a message when the command /start is issued."""
-    full_name = update.message.from_user.full_name 
-    message_start = (f"Boas vindas, {full_name}, ao bot CVE_search\n\n"
-            "Comandos:\n\n /search vendor(or product) para procurar por cves\n\n /id to search byt CVE-ID  \n\n /last para exibir o ultimo registro disponível \n\n /help para exibir os comandos")
-    update.message.reply_text
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
-def help(bot, update):
-    message.help = (
-            " Comandos: " 
-            "/search procura um produto especifico\n"
-            "/id procura por uma CVE-ID\n"
-            "/last mostra o ultimo registro disponível\n"
-            "/help para exbir esta mensagem :sunglasses:\n"
-            )
-    update.message.reply_text(message_help)
+# Motor
+def main():
+    """Start the bot."""
+    updater = Updater(environ['TOKEN'])
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("search", search))
+    dp.add_error_handler(error)
 
-def description(bot, update):
-    message_description = ( 
-            " Telegram bot para procurar por  CVE's, totalmente sotware livre \n"
-            " Para saber mais sobre CVE's e sua definição visite: https://www.redhat.com/pt-br/topics/security/what-is-cve \n" 
-            " Criticas e Pull Requests são bem-vindos, @0l1v3r2 para falar diretamente comigo :smile: ou visite nosso repositorio: https://github.com/matholiveira91/cve_search_telegram_bot \n" )
-    update.message.reply_text(message_desciption)
+    updater.start_polling()
+    updater.idle()
+~
 
+if __name__ == '__main__':
+    main()
 
             
 
