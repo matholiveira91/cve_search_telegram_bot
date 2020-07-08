45da118e8b5c0684be09d37a1be1237d119fa63f
matholiveira91 <matheusoliveiratux4me@gmail.com>
2020-07-07 21:59:19 -0300

    remove advisories :sad:

--- cve.py
+++ cve.py
@@ -49,30 +49,30 @@ params = 'package=kernel&after=' + str(date.date())
 
 data = get_data(endpoint + '?' + params)
 
-kernel_advisories = []
-for advisory in data:
-    print(advisory['RHSA'], advisory['severity'], advisory['released_on'])
-    print('-', '\n- '.join(advisory['released_packages']))
-    kernel_advisories.append(advisory['RHSA'])
+#kernel_advisories = []
+#for advisory in data:
+ #   print(advisory['RHSA'], advisory['severity'], advisory['released_on'])
+  #  print('-', '\n- '.join(advisory['released_packages']))
+   # kernel_advisories.append(advisory['RHSA'])
 
 
-print('-----')
+#print('-----')
 # From the list of advisories saved in the previous example (as
 # `kernel_advisories`), get a list of affected products for each advisory.
-endpoint = '/cvrf/'
+#endpoint = '/cvrf/'
 
-for advisory in kernel_advisories:
-    data = get_data(endpoint + advisory + '.json')
-    print(advisory)
+#for advisory in kernel_advisories:
+#    data = get_data(endpoint + advisory + '.json')
+#    print(advisory)
 
-    product_branch = data['cvrfdoc']['product_tree']['branch']
-    for product_branch in data['cvrfdoc']['product_tree']['branch']:
+#    product_branch = data['cvrfdoc']['product_tree']['branch']
+#    for product_branch in data['cvrfdoc']['product_tree']['branch']:
 
-        if product_branch['type'] == 'Product Family':
+#        if product_branch['type'] == 'Product Family':
 
-            if type(product_branch['branch']) is dict:
-                print('-', product_branch['branch']['full_product_name'])
+#            if type(product_branch['branch']) is dict:
+#                print('-', product_branch['branch']['full_product_name'])
 
-            else:
-                print('-', '\n- '.join(pr['full_product_name'] for
-                                       pr in product_branch['branch']))
+ #           else:
+ #               print('-', '\n- '.join(pr['full_product_name'] for
+#                                       pr in product_branch['branch']))
